fe9fc096a68f8fbd127bd6992ac967da5951107f
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-01-20 19:15:25 -0300

    criando estrutura de cardápio

--- oficial-lanches/src/componentes/cardapio/index.jsx
+++ oficial-lanches/src/componentes/cardapio/index.jsx
@@ -17,7 +17,232 @@ export default () => {
                 <div className={styles.cardapio_box}>
                     <div className={styles.cardapio}>
                         <div className={styles.menu}>
-
+                            <div className={styles.tabBox}>
+                                <button type="button" className={`${styles.btn}`}>Salgados</button>
+                            </div>
+                            <div className={styles.tabBox}>
+                            <button type="button" className={`${styles.btn} ${styles.hiddenBorder}`}>Bebidas</button>
+                            </div>
+                            <div className={styles.tabBox}>
+                                <button type="button" className={`${styles.btn} ${styles.hiddenBorder}`}>Doces</button>
+                            </div>
+                            <div className={styles.tabBox}>
+                                <button type="button" className={`${styles.btn} ${styles.hiddenBorder}`}>Sobre</button>
+                            </div>
+                        </div>
+                        <div className={`${styles.grupo} ${styles.hidden}`}>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.grupo} ${styles.hidden}`}>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.grupo} ${styles.hidden}`}>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.item}>
+                                <img src="" alt="" />
+                                <div className={styles.description}>
+                                    <div className={styles.texto}>
+                                        <h3 className={styles.title}></h3>
+                                        <p></p>
+                                        <h4 className={styles.preco}></h4>
+                                    </div>
+                                    <div className={styles.btnBox}>
+                                        <button className={styles.adicionar} data-item="">Adicionar</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>

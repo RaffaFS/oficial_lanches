2c1d4e2f42802d94339b10a1d07ba96bcd56e14c
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-01-20 18:07:59 -0300

    preparando o ambiente

---
+++ oficial-lanches/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cardapio from "./componentes"
+
+function App() {
+
+  return (
+    <>
+      <Cardapio />
+    </>
+  )
+}
+
+export default App
---
+++ oficial-lanches/src/componentes/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from './form.module.css';
+
+export default () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
